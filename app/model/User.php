1e935782268eea8d55ad1f97064ed9f679ca4aeb
Lorenzo Malferrari <lorenzo.malferrari@ag2.it>
2024-03-27 17:14:01 +0100

    Applicate migliorie al progetto

--- app/model/User.php
+++ app/model/User.php
@@ -1,54 +1,49 @@
 <?php
-    /**
-     *
-     */
-    class User {
+	/**
+	 * Classe che rappresenta l'Utente
+	 */
+	class User {
 
-        // Variable for user ID
-        private int $id;
-        // Instance variables
-        protected $username;
-        protected $email;
+		// Variable for user ID
+		private int $id;
+		// Instance variables
+		protected $username;
+		protected $password;
 
-        // Constructor
-        public function __construct($username, $email) {
-            $this->username = $username;
-            $this->email = $email;
-        }
+		// Constructor
+		public function __construct($username, $password) {
+			$this->username = $username;
+			$this->password = $password;
+		}
 
-        // Getter for user ID
-        public function getId(): int {
-            return $this->id;
-        }
+		// Getter for user ID
+		public function getId(): int {
+			return $this->id;
+		}
 
-        // Setter for user ID
-        public function setId(int $id): void {
-            $this->id = $id;
-        }
+		// Getter and Setter for username
+		public function getUsername() {
+			return $this->username;
+		}
 
-        // Getter and Setter for username
-        public function getUsername() {
-            return $this->username;
-        }
+		public function setUsername($username) {
+			$this->username = $username;
+		}
 
-        public function setUsername($username) {
-            $this->username = $username;
-        }
+		// Getter and Setter for email
+		public function getPassword() {
+			return $this->password;
+		}
 
-        // Getter and Setter for email
-        public function getEmail() {
-            return $this->email;
-        }
+		public function setPassword($password) {
+			$this->password = $password;
+		}
 
-        public function setEmail($email) {
-            $this->email = $email;
-        }
-
-        // Function to print user information
-        public function __toString() {
-            echo "Id: " . $this->id . "\n";
-            echo "Username: " . $this->username . "\n";
-            echo "Email: " . $this->email . "\n";
-        }
-    }
+		// Function to print user information
+		public function __toString() {
+			echo "Id: " . $this->id . "\n";
+			echo "Username: " . $this->username . "\n";
+			echo "Password: " . $this->password . "\n";
+		}
+	}
 ?>
