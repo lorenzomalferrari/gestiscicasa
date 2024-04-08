6c208c368d171881ff0d5a8eeff1c73ab095f1a9
Lorenzo Malferrari <lorenzo.malferrari@ag2.it>
2024-04-08 15:29:51 +0200

    Corretta classe Color, introdotto file _config per la gestione dei dati e predisposto index.php

--- app/model/Colors.php
+++ app/model/Colors.php
@@ -14,7 +14,7 @@
 		private string $note;
 		private string $creationDate;
 		private string $updateDate;
-	
+
 		public function __construct(int $id, string $code, string $name, string $hexCode, string $rgbCode, string $rgbaCode, string $hslCode, string $creationDate, string $updateDate, string $note=null) {
 			$this->id = $id;
 			$this->code = $code;
@@ -27,87 +27,86 @@
 			$this->creationDate = $creationDate;
 			$this->updateDate = $updateDate;
 		}
-	
+
 		public function getId(): int {
 			return $this->id;
 		}
-	
+
 		public function getCode(): string {
 			return $this->code;
 		}
-	
+
 		public function setCode(string $code): void {
 			$this->code = $code;
 		}
-	
+
 		public function getName(): string {
 			return $this->name;
 		}
-	
+
 		public function setName(string $name): void {
 			$this->name = $name;
 		}
-	
+
 		public function getHexCode(): string {
 			return $this->hexCode;
 		}
-	
+
 		public function setHexCode(string $hexCode): void {
 			$this->hexCode = $hexCode;
 		}
-	
+
 		public function getRgbCode(): string {
 			return $this->rgbCode;
 		}
-	
+
 		public function setRgbCode(string $rgbCode): void {
 			$this->rgbCode = $rgbCode;
 		}
-	
+
 		public function getRgbaCode(): string {
 			return $this->rgbaCode;
 		}
-	
+
 		public function setRgbaCode(string $rgbaCode): void {
 			$this->rgbaCode = $rgbaCode;
 		}
-	
+
 		public function getHslCode(): string {
 			return $this->hslCode;
 		}
-	
+
 		public function setHslCode(string $hslCode): void {
 			$this->hslCode = $hslCode;
 		}
-	
+
 		public function getNote(): string {
 			return $this->note;
 		}
-	
+
 		public function setNote(string $note): void {
 			$this->note = $note;
 		}
-	
+
 		public function getCreationDate(): string {
 			return $this->creationDate;
 		}
-	
+
 		public function setCreationDate(string $creationDate): void {
 			$this->creationDate = $creationDate;
 		}
-	
+
 		public function getUpdateDate(): string {
 			return $this->updateDate;
 		}
-	
+
 		public function setUpdateDate(string $updateDate): void {
 			$this->updateDate = $updateDate;
 		}
-	
+
 		public function __toString(): string {
 			return "ID: {$this->id}, Code: {$this->code}, Name: {$this->name}, Hex Code: {$this->hexCode}, RGB Code: {$this->rgbCode}, RGBA Code: {$this->rgbaCode}, HSL Code: {$this->hslCode}, Note: {$this->note}, Creation Date: {$this->creationDate}, Update Date: {$this->updateDate}";
 		}
 	}
-	
 
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
-    $vers = time();
+    $vers = time();//predispongo variabile per il timing
 
+    //se $_GET è settata ed è un array lo copio in $_POST, necessario per la personalizzazione del gestionale
     if( isset($_GET) && is_array($_GET)){
         $_POST = $_GET;
     }
 
+    //controllo sessione
+    //se sono già loggato vado in home
+
+    //se non sono già loggato o ho perso la sessione vado in login
     //require_once('app/controller/login.php');
     require_once('app/view/login.html');
 ?>
