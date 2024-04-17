308022e9909fc280ff6aa8e52d21ecf6c7e7a7b1
Lorenzo Malferrari <lorenzo.malferrari@ag2.it>
2024-04-17 10:02:54 +0200

    Rinominato file di configurazione e corretto punti nella pagina di attracco home

--- app/controller/home.php
+++ app/controller/home.php
@@ -1,15 +1,19 @@
 <?php
+	require("_config.php");
 	//recuperare id dell' utente, iterrogare il database e recuperare le informazioni da mostrare a video negli appositi spazi
 	$idUtente_se = $_SESSION["idUtente"];
 
+	$table = $config['db']['tablePrefix'] . "User";
 	$getUser_sql = <<<sql
 		SELECT
 			*
 		FROM
-			User
+			$table
 		WHERE
 			id = ?
 sql;
+	print_r($getUser_sql);
+	
 	// Ottieni il nome utente dall'autenticazione (sostituire con il tuo sistema di autenticazione)
 	$username = $_SESSION["username"];
 	$email = $_SESSION["email"];
