47606a1c66dcfa23825da7ef41acf581f4f4ef0e
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-04-27 21:14:02 +0200

    Creato file per il require massivo di tutte le libs importanti:

---
+++ app/controller/_libs.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * File da richiamare ovunque, il quale conterrà
+     * tutti i richiami dei file importati per la gestione del progetto:
+     *
+     * _config.php: parametri utili per la connessione al database e altri parametri di sicurezza
+     * _functions.php: funzioni utili per la gestione di controlli
+     */
+
+    require_once("_config.php");
+    require_once("_functions.php");
+?>
