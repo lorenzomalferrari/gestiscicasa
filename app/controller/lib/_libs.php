b3d6ce2b3ecd8080edcdc18c0e6f6c2cd1d4a160
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-05-15 17:09:58 +0200

    Aggiunta richiamo nuovo file

--- app/controller/lib/_libs.php
+++ app/controller/lib/_libs.php
@@ -1,19 +1,19 @@
 <?php
-//echo "Sono _libs.php";
-/**
- * File da richiamare ovunque, il quale conterrà
- * tutti i richiami dei file importati per la gestione del progetto:
- *
- * _config.php: parametri utili per la connessione al database e altri parametri di sicurezza
- * _db.php: parametri utili per la connessione al database
- * _def.php: Contiene le variabili GLOBALI CUSTOM, alcune dipendono dal file _config.php
- * _time.php: Ottengo time per gestire il refresh della cace con le lib css e js
- * _getYear.php: Ottengo l'anno attuale
- * _functions.php: funzioni utili per la gestione di controlli
- * _encryption.php: contiene le funzioni per criptare e descriptare testi
- * _get_set.php: sposta tutti i parametri da GET a SET
- * _getToken.php: genero un token univoco
- */
+    /**
+     * File da richiamare ovunque, il quale conterrà
+     * tutti i richiami dei file importati per la gestione del progetto:
+     *
+     * _config.php: parametri utili per la connessione al database e altri parametri di sicurezza
+     * _db.php: parametri utili per la connessione al database
+     * _def.php: Contiene le variabili GLOBALI CUSTOM, alcune dipendono dal file _config.php
+     * _time.php: Ottengo time per gestire il refresh della cace con le lib css e js
+     * _getYear.php: Ottengo l'anno attuale
+     * _functions.php: funzioni utili per la gestione di controlli
+     * _encryption.php: contiene le funzioni per criptare e descriptare testi
+     * _get_set.php: sposta tutti i parametri da GET a SET
+     * _getToken.php: genero un token univoco
+     * _setStatiCRUD.php: interroga il DB e predisposne le costanti per la gestione delle operazioni CRUD
+     */
 
     session_start();
 
@@ -26,4 +26,5 @@
     require_once("_encryption.php");
     require_once("_get_set.php");
     require_once("_getToken.php");
+    require_once("_setStatiCRUD.php")
 ?>
