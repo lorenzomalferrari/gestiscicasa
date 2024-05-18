0740914636fd131cb81d2b31d791128a1c536ec2
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-05-18 23:57:58 +0200

    Corretto voci nelle query

--- public/_dati/db/User.sql
+++ public/_dati/db/User.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS lmgc_Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NOT NULL,
     creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updateData TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -13,3 +13,5 @@ ADD CONSTRAINT UC_Username UNIQUE (username); -- Assicura che lo username sia un
 ALTER TABLE lmgc_Users
 ADD COLUMN token VARCHAR(255),
 ADD COLUMN isActive bit DEFAULT b'1';
+
+ALTER TABLE lmgc_Users DROP COLUMN email;
