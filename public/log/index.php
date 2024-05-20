8d6f54b58797ed4d5cdd14e78856f8f0b71dfbae
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-05-20 23:22:13 +0200

    Update index.php

---
+++ public/log/index.php
@@ -0,0 +1,3 @@
+cartella che conterrà i file di log. saranno raggruppati per cartelle e saranno anche presenti degli zip.
+
+Questo file interrogato a video restituisce questo testo seguito da uno script in json.
