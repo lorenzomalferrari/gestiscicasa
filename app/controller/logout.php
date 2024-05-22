bed11cdc9d7c4356868b980af3f6d0975631d2bf
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-05-22 18:31:01 +0200

    Puliti spazi

--- app/controller/logout.php
+++ app/controller/logout.php
@@ -1,10 +1,8 @@
 <?php
     // Pulisci tutte le variabili di sessione
     session_unset();
-
     // Distruggi la sessione
     session_destroy();
-
     // Reindirizza l'utente alla pagina di accesso o ad un'altra pagina
     header("Location: login.php");
 ?>
