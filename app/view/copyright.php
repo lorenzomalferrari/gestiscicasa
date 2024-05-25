6107ce98eea95d2f5ad1291c53fd7c825470aae7
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-05-25 09:15:22 +0200

    Corretta rinominazione dei file

---
+++ app/view/copyright.php
@@ -0,0 +1,9 @@
+<?php
+    require_once("../controller/lib/_libs.php");
+?>
+<div class="copyright-text">
+    © <?php echo $YEARNOW; ?>
+    <span><?php echo $config['site']['name']; ?></span>
+    - Autore: <span><?php echo $config['site']['autore']; ?></span>
+    - Versione: <span><?php echo $config['site']['version']; ?></span>
+</div>
---
+++ app/view/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer-wrap">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="copyright-text">
+                    © <?php echo $YEARNOW; ?> <span><?php echo $config['site']['name']; ?></span>. <?php echo $config['site']['autore']; ?> - Versione: <span><?php echo $config['site']['version']; ?></span>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <ul class="footer-link">
+                    <li><a href="#">Terms</a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Help</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
