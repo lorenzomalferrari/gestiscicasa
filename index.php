56fc5f5f7674f2224258225862e2372658e277ea
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-05-25 09:19:37 +0200

    in debug la sessione

--- index.php
+++ index.php
@@ -7,10 +7,15 @@
 	require_once("app/controller/lib/_libs.php");
 	//require_once("app/model/session.php");
 
+	$username = $_SESSION["USER_SE"];
+	$idUsername = $_SESSION["IDUSER_SE"];
+
 	if (!checkUsernameAndId($username, $idUsername)) {
-		header('Location: ' . 'app/view/login.php');
+		print_r('Location: ' . 'app/view/login.php');
+		//header('Location: ' . 'app/view/login.php');
 	}
 	else {
-		header('Location: ' . 'app/view/home.php');
+		print_r('Location: ' . 'app/view/home.php');
+		//header('Location: ' . 'app/view/login.php');
 	}
 ?>
