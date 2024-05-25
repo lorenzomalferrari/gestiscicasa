3a4dfbb1eb7dbbaf3b8df4f580249ebc920b75ce
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-05-25 09:18:08 +0200

    da eliminare

--- app/model/session.php
+++ app/model/session.php
@@ -1,10 +1,10 @@
 <?php
-    session_start();
+    //session_start();
 
-    if (isset($_SESSION)) {
-        $USER_SE = $_SESSION["USER_SE"];
-        $IDUSER_SE = $_SESSION["IDUSER_SE"];
-    }
+    //if (isset($_SESSION)) {
+        //$USER_SE = $_SESSION["USER_SE"];
+        //$IDUSER_SE = $_SESSION["IDUSER_SE"];
+    //}
 
     //echo "<br><br>USER_SE: $USER_SE - IDUSER_SE: $IDUSER_SE<br><br>";
 ?>
