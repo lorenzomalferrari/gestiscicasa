172878c136fe92d1dcdc79ef1cc20ac8c2c984a1
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-05-25 09:17:47 +0200

    Migliorato controllo Sessione

--- app/controller/lib/_setSession.php
+++ app/controller/lib/_setSession.php
@@ -1,19 +1,34 @@
 <?php
-    // Verifica se la sessione è già stata avviata
-    if (!isset($_SESSION)) {
-        //print_r("   sono dentro, devo pulire e riniziare   ---");
-        // Pulisci tutte le variabili di sessione
-        session_unset();
-        // Avvia la sessione
-        session_start();
-        $_SESSION = array();//al momento funziona solo facendo così
-        //print_r($_SESSION);
-    }else{
-        $_SESSION["IDUSER_SE"] = $row["id"];
-        $_SESSION["USER_SE"] = $row["username"];
-        $_SESSION["PASSWORD_SE"] = $row["password"];
-        $_SESSION["EMAIL_SE"] = $row["email"];
-    }
+    // Avvia la sessione
+    session_start();
 
-    //print_r($_SESSION);
+    print_r("Sessione settata: -" . isset($_SESSION) . "-<br>");
+    print_r($_SESSION);
+
+    $keys = $config['session']['keys'];
+print_r(var_export(array_has_any_key($_SESSION, $keys)));
+print_r('<br><br><br>');
+    // Verifica se la sessione contiene già le key utili nel CRM
+    if ( array_has_any_key($_SESSION, $keys)) {
+        //se il token è ancora valido
+        if(checkArrayValues($_SESSION)){
+
+        }
+        else{ //pulire sessione e forzare utente al ritorno su login.php
+            print_r("   sono dentro, devo pulire e riniziare   ---");
+            // Pulisci tutte le variabili di sessione
+            session_unset();
+            $_SESSION = array(); //al momento funziona solo facendo così
+            header("Location: " . "../view/login.php");
+        }
+    }else{
+        print_r('preparo key utili nel CRM');
+        foreach ($keys as $key) {
+            $_SESSION[$key] = "";
+        }
+    }
+    print_r('<br><br><br>');
+    print_r($_SESSION);
+print_r('<br><br><br>');
+print_r('esco da _setSessione.php');
 ?>
