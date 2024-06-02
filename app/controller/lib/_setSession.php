0f81b948b07c4fdcd05e3b586ed6e0d03fd4fe8b
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-06-02 21:58:16 +0200

    in lavorazione

--- app/controller/lib/_setSession.php
+++ app/controller/lib/_setSession.php
@@ -1,25 +1,31 @@
 <?php
-    // Avvia la sessione
-    session_start();
+    //DA CORREGGERE E FAR FUNZIONARE ASSOLUTAMENTE
+    // Avvia la sessione se non settata
+    if(!isset($_SESSION))
+        session_start();
 
-    print_r("Sessione settata: -" . isset($_SESSION) . "-<br>");
-    print_r($_SESSION);
+    //print_r("Sessione settata: -" . isset($_SESSION) . "-<br>");
+    //print_r($_SESSION);
 
     $keys = $config['session']['keys'];
-print_r(var_export(array_has_any_key($_SESSION, $keys)));
-print_r('<br><br><br>');
+    //print_r(var_export(array_has_any_key($_SESSION, $keys)));
+    //print_r('<br><br><br>');
     // Verifica se la sessione contiene già le key utili nel CRM
     if ( array_has_any_key($_SESSION, $keys)) {
-        //se il token è ancora valido
-        if(checkArrayValues($_SESSION)){
-
+        print_r("dentro a if,quindi ho già le key settate");
+        //se il token è ancora valido, mantengo sessione
+        //si prosegue in pagina interessata
+        if(1 == 3){
+            //niente da fare
         }
-        else{ //pulire sessione e forzare utente al ritorno su login.php
-            print_r("   sono dentro, devo pulire e riniziare   ---");
+        else{ // sennò pulire sessione e forzare ritorno al login 
+            //pulire sessione e forzare utente al ritorno su login.php
+            //print_r("   sono dentro, devo pulire e riniziare   ---");
             // Pulisci tutte le variabili di sessione
-            session_unset();
             $_SESSION = array(); //al momento funziona solo facendo così
-            header("Location: " . "../view/login.php");
+            session_unset();
+            //header("Location: " . "../view/login.php");
+            print_r("Location: " . "../view/login.php");
         }
     }else{
         print_r('preparo key utili nel CRM');
@@ -27,8 +33,8 @@ print_r('<br><br><br>');
             $_SESSION[$key] = "";
         }
     }
-    print_r('<br><br><br>');
+    //print_r('<br><br><br>');
     print_r($_SESSION);
-print_r('<br><br><br>');
-print_r('esco da _setSessione.php');
+    print_r('<br><br><br>');
+    //print_r('esco da _setSessione.php');
 ?>
