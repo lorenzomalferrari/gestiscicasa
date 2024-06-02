0609efc6e38fcdf03709331c4353aaf4977cae7b
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-06-02 21:55:21 +0200

    Corretta tabulazione commento

--- app/controller/lib/_libs.php
+++ app/controller/lib/_libs.php
@@ -1,21 +1,22 @@
 <?php
-
-/**
- * File da richiamare ovunque, il quale conterrà
- * tutti i richiami dei file importati per la gestione del progetto:
- *
- * _config.php: parametri utili per la connessione al database e altri parametri di sicurezza
- * _db.php: parametri utili per la connessione al database
- * _def.php: Contiene le variabili GLOBALI CUSTOM, alcune dipendono dal file _config.php
- * _time.php: Ottengo time per gestire il refresh della cace con le lib css e js
- * _getYear.php: Ottengo l'anno attuale
- * _functions.php: funzioni utili per la gestione di controlli
- * _encryption.php: contiene le funzioni per criptare e descriptare testi
- * _get_set.php: sposta tutti i parametri da GET a SET
- * _getToken.php: genero un token univoco
- * _setSession.php: Configura la Sessione
- * _setStatiCRUD.php: interroga il DB e predisposne le costanti per la gestione delle operazioni CRUD
- */
+    /**
+     * File da richiamare ovunque, il quale conterrà
+     * tutti i richiami dei file importati per la gestione del progetto:
+     *
+     *      _config.php: Parametri utili per la connessione al database e altri parametri di sicurezza
+     *      _db.php: Parametri utili per la connessione al database
+     *      _def.php: Contiene le variabili GLOBALI CUSTOM, alcune dipendono dal file _config.php
+     *      _time.php: Ottengo time per gestire il refresh della cace con le lib css e js
+     *      _getYear.php: Ottengo l'anno attuale
+     *      _functions.php: Funzioni utili per la gestione di controlli
+     *      _encryption.php: Contiene le funzioni per criptare e descriptare testi
+     *      _get_set.php: Sposta tutti i parametri da GET a SET, sarà utile per mod e personalizzazioni
+     *      _getToken.php: Genero un token univoco (in lavorazione vers 2 potenziata)
+     *      _setSession.php: Configura la Sessione (problemi da risolvere)
+     *      _setStatiCRUD.php: Interroga il DB e predisposne le costanti per la gestione delle operazioni CRUD
+     *      -> potrebbe essere utile creare sotto cartella che oltre a StatiCRUD setta altre informazioni utili e necessarie 
+     *      (da pensare per bene, infatti al momento il file è commentato)
+     */
 
     require_once("_config.php");
     require_once("_db.php");
