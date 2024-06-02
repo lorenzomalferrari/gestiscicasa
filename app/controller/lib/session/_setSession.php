370cddd55212ebb4fef9e156afbb45783d9e0302
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-06-03 00:17:49 +0200

    Migliorata lib della SESSIONE, ancora da lavorarci

--- app/controller/lib/session/_setSession.php
+++ app/controller/lib/session/_setSession.php
@@ -12,25 +12,36 @@
     //print_r($_SESSION);
 
     $keys = $config['session']['keys'];
-    //print_r(var_export(array_has_any_key($_SESSION, $keys)));
-    //print_r('<br><br><br>');
+    print_r(var_export(array_has_any_key($_SESSION, $keys)));
+    print_r('<br><br><br>');
     // Verifica se la sessione contiene già le key utili nel CRM
     if ( array_has_any_key($_SESSION, $keys)) {
         print_r("dentro a if,quindi ho già le key settate");
-        //se il token è ancora valido, mantengo sessione
-        //si prosegue in pagina interessata
+    //se il token è ancora valido, mantengo sessione
+    //si prosegue in pagina interessata
+    print_r("IDUSER: " . isNotNullOrEmpty($_SESSION['IDUSER']));
+    print_r("USERNAME: " . isNotNullOrEmpty($_SESSION['USERNAME']));
+    print_r("PASSWORD: " . isNotNullOrEmpty($_SESSION['PASSWORD']) . "<br>");
+    print_r("EMAIL: " . isNotNullOrEmpty($_SESSION['EMAIL']) . "<br>");
+    print_r("TOKEN: " . isNotNullOrEmpty($_SESSION['TOKEN'])  . "<br>" );
+
         if(
             isNotNullOrEmpty($_SESSION['IDUSER']) &&
             isNotNullOrEmpty($_SESSION['USERNAME']) &&
             isNotNullOrEmpty($_SESSION['PASSWORD']) &&
-            isNotNullOrEmpty($_SESSION['EMAIL']) &&
-            isNotNullOrEmpty($_SESSION['TOKEN'])
+            isNotNullOrEmpty($_SESSION['EMAIL']) /*&&
+            isNotNullOrEmpty($_SESSION['TOKEN']) */
         ){
             //niente da fare
             print_r('Non faccio niente, sessione ok, andrò in Home');
         }
-        else{ // sennò pulire sessione e forzare ritorno al login
-            //require_once("session_destroy.php");
+        else{ //ho fatto registrazione
+            //setto voci e vado in home
+            //print_r('Compilo SESSION con i dati del nuovo utente registrato:');
+            //print_r($row);
+            //if(isset($row))
+            //{
+            //}
         }
     }else{
         print_r('preparo key utili nel CRM');
