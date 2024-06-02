f9005543c5754c6cf8bd53163489bd6977cfba60
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-06-02 21:56:07 +0200

    Sistemato file

--- app/controller/lib/_def.php
+++ app/controller/lib/_def.php
@@ -1,23 +1,28 @@
 <?php
-    //echo "Sono _def.php";
     /**
-     * File che contiene tutte le VARIABILI GLOBALI CUSTOM
+     * File dove definisco alcune VARIABILI GLOBALI CUSTOM
      */
 
-    define("HTTP_HOST", $_SERVER["HTTP_HOST"]);//es.
-    define("PROTOCOL", $_SERVER["REQUEST_SCHEME"]);//es. http vs https
-    define("EMAIL_SERVER_ADMIN", $_SERVER["SERVER_ADMIN"] . "/");//es.
-    define("DOMINIO", $_SERVER["SERVER_NAME"]); //es.
-    define("ROOT", $_SERVER["DOCUMENT_ROOT"] . "/");//es.
-    define("PATH", PROTOCOL ."://" . DOMINIO . "/");//es.
-    // Chiave per la crittografia
-    define('CRYPTO_KEY', ($config['db']['tablePrefix'] . $config['securityConfig']['key']));//es. lmgc_1e2wfvdfyku65yt4efbntmyj
-
+    define("HTTP_HOST", $_SERVER["HTTP_HOST"]);
     //echo "<br>HTTP_HOST:" . HTTP_HOST;
+    define("PROTOCOL", $_SERVER["REQUEST_SCHEME"]);//es. http vs https
     //echo "<br>PROTOCOL:" . PROTOCOL;
+    define("EMAIL_SERVER_ADMIN", $_SERVER["SERVER_ADMIN"] . "/");
     //echo "<br>EMAIL_SERVER_ADMIN_" . EMAIL_SERVER_ADMIN;
+    define("DOMINIO", $_SERVER["SERVER_NAME"]); //es. gestiscicasa.it
     //echo "<br>DOMINIO:" . DOMINIO;
-    //echo "<br>PATH:" . PATH;
+    define("ROOT", $_SERVER["DOCUMENT_ROOT"] . "/");
     //echo "<br>ROOT:" . ROOT;
+    define("PATH", PROTOCOL ."://" . DOMINIO . "/");
+    //echo "<br>PATH:" . PATH;
+    // Chiave per la crittografia composta da voci interne
+    //VEDERE SE AGGIUNGERE ULTERIORI CAMPI
+    define(
+        'CRYPTO_KEY',
+        (
+            $config['db']['tablePrefix'] .
+            $config['securityConfig']['key']
+        )
+    );//es. lmgc_1e2wfvdfyku65yt4efbntmyj
     //echo "<br>CRYPTO_KEY:" . CRYPTO_KEY;
 ?>
