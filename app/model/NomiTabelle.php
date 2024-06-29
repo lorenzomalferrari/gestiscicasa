a5a8ce84d1b2e46916233719491180b281f1a3b8
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-06-30 00:46:38 +0200

    rimosso require

--- app/model/NomiTabelle.php
+++ app/model/NomiTabelle.php
@@ -1,7 +1,5 @@
 <?php declare(strict_types=1);
 
-    require_once('../controller/lib/_libs.php');
-
     class TableNames {
         public static function getPrefixedName(string $prefisso, string $tableName): string
         {
