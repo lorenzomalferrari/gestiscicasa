87952e9fe204b67360f23a52df2c59b85b02eb1c
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-06-30 00:22:30 +0200

    Controllato uso della funzione e rimosso debug

--- app/controller/lib/_db.php
+++ app/controller/lib/_db.php
@@ -1,9 +1,16 @@
 <?php declare(strict_types=1);
     global $SERVERNAME_DB, $USERNAME_DB, $PASSWORD_DB, $DBNAME, $TABLEPREFIX;
 
-    $SERVERNAME_DB = $config['db']['test']['host'] . ':' . $config['db']['test']['port'];
-    $USERNAME_DB = $config['db']['test']['username'];
-    $PASSWORD_DB = $config['db']['test']['password'];
-    $DBNAME = $config['db']['test']['database'];
+    $key = "produzione";
+    if( isLocalhost() ){
+        $key = "test";
+    }
+
+    print_r("key: " . $key);
+
+    $SERVERNAME_DB = $config['db'][$key]['host'] . ':' . $config['db'][$key]['port'];
+    $USERNAME_DB = $config['db'][$key]['username'];
+    $PASSWORD_DB = $config['db'][$key]['password'];
+    $DBNAME = $config['db'][$key]['database'];
     $TABLEPREFIX = $config['db']['tablePrefix'];
 ?>
