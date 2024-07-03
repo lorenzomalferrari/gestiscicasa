2776897aec180207cde4f7e80283baf4a4f3aa60
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-07-03 13:51:21 +0200

    Agg tonda per err dovuto da sostituzione massiva

--- app/controller/lib/get_set/_setStatiCRUD.php
+++ app/controller/lib/get_set/_setStatiCRUD.php
@@ -6,7 +6,7 @@
     // Array che conterrà gli oggetti CrudType
     global $CRUDTYPEOBJ;// vedere se portarlo in _def.php
 
-    $database = new Database($configIstance->get('SERVERNAME_DB'), $configIstance->get('USERNAME_DB'), $configIstance->get('PASSWORD_DB'), $configIstance->get('DBNAME');
+    $database = new Database($configIstance->get('SERVERNAME_DB'), $configIstance->get('USERNAME_DB'), $configIstance->get('PASSWORD_DB'), $configIstance->get('DBNAME'));
     // Preparazione della query
     $params = array();
     //Controllo prima che Utente non esista già
--- app/model/Profile.php
+++ app/model/Profile.php
@@ -20,7 +20,7 @@
          */
         public static function init(): void
         {
-            self::$db = new Database($configIstance->get('SERVERNAME_DB'), $configIstance->get('USERNAME_DB'), $configIstance->get('PASSWORD_DB'), $configIstance->get('DBNAME');
+            self::$db = new Database($configIstance->get('SERVERNAME_DB'), $configIstance->get('USERNAME_DB'), $configIstance->get('PASSWORD_DB'), $configIstance->get('DBNAME'));
         }
 
         /**
