b3ab264903053e471f6c4fe22aa651df43b7aea7
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-07-11 16:57:13 +0200

    Creata classe per leggere e scrivere su file

---
+++ app/model/file/FileManager.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Class FileManager
+ *
+ * Classe per la gestione della scrittura e lettura da file.
+ */
+class FileManager
+{
+    /**
+     * Scrive dei dati in un file.
+     *
+     * @param string $filename Nome del file in cui scrivere i dati.
+     * @param string $data Dati da scrivere nel file.
+     * @param bool $append True se si desidera aggiungere i dati al file esistente, False per sovrascrivere il file.
+     * @return bool True se la scrittura è avvenuta con successo, False altrimenti.
+     */
+    public static function writeToFile($filename, $data, $append = false)
+    {
+        $flags = $append ? FILE_APPEND | LOCK_EX : LOCK_EX;
+        return file_put_contents($filename, $data, $flags) !== false;
+    }
+
+    /**
+     * Legge i dati da un file.
+     *
+     * @param string $filename Nome del file da cui leggere i dati.
+     * @return string|false Contenuto del file letto, oppure False in caso di errore.
+     */
+    public static function readFromFile($filename)
+    {
+        if (file_exists($filename) && is_readable($filename)) {
+            return file_get_contents($filename);
+        }
+        return false;
+    }
+}
