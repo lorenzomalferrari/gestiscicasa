1089738091bd58a555d8f312557155786847d07e
Lorenzo Malferrari <lorenzo.malferrari@ag2.it>
2024-07-12 18:01:56 +0200

    Migliorato commento della lib

--- app/controller/lib/libs.php
+++ app/controller/lib/libs.php
@@ -1,15 +1,23 @@
 <?php declare(strict_types=1);
+
     /**
      * File da richiamare ovunque, il quale conterrà
      * tutti i richiami dei file importati per la gestione del progetto:
      *
      *      _define.php: Contiene le definizioni utili per la ROOT e PATH e CONFIG
      *      _functions.php: Funzioni utili per la gestione di controlli
+     *      _create_directories.php: Recupera una funzione per creare e gestire file e cartelle
      *      _globals.php: Definisce variabili GLOBALI utili ad esempio al DB
+     *
      *      __session_start.php: Gestisce la sessione
      *
      *      Tables.php: Per ogni tabella a DB esiste una classe con costanti per richiamare nome della tabella e i campi, evitando errori di battitura
      *
+     *      Database.php: Uso della lib per la connessione al DB
+     *
+     *      Log.php: Uso della lib per la gestione di tutti i Log del caso
+     *
+     *      CustomException.php: Uso della lib per la gesione custom delle eccezioni
      */
     require_once("define/_define.php");
     require_once("functions/_functions.php");
@@ -31,12 +39,10 @@
     ));
 
     require_once(ROOT . 'app/model/log/Log.php');
+    require_once(ROOT . 'app/model/CustomException.php');
 
     //verifico o creo le cartelle dei logs
-    $msg = (string) createDirectories(CONFIG);
-    if( strlen($msg) > 1 ){
-
-    }
+    createDirectories(CONFIG);
 
     DB->checkDatabaseVersion();//se non vengono generati errori, si prosegue
 
