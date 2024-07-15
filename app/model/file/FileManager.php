9b73ac538d67fb8f6ae2702a2d9b4399cce6939f
Lorenzo Malferrari <lorenzo.malferrari@ag2.it>
2024-07-15 14:18:25 +0200

    corretta tabulazione

--- app/model/file/FileManager.php
+++ app/model/file/FileManager.php
@@ -1,37 +1,37 @@
-<?php
-
-/**
- * Class FileManager
- *
- * Classe per la gestione della scrittura e lettura da file.
- */
-class FileManager
-{
-    /**
-     * Scrive dei dati in un file.
-     *
-     * @param string $filename Nome del file in cui scrivere i dati.
-     * @param string $data Dati da scrivere nel file.
-     * @param bool $append True se si desidera aggiungere i dati al file esistente, False per sovrascrivere il file.
-     * @return bool True se la scrittura è avvenuta con successo, False altrimenti.
-     */
-    public static function writeToFile($filename, $data, $append = false)
-    {
-        $flags = $append ? FILE_APPEND | LOCK_EX : LOCK_EX;
-        return file_put_contents($filename, $data, $flags) !== false;
-    }
+<?php declare(strict_types=1);
 
     /**
-     * Legge i dati da un file.
+     * Class FileManager
      *
-     * @param string $filename Nome del file da cui leggere i dati.
-     * @return string|false Contenuto del file letto, oppure False in caso di errore.
+     * Classe per la gestione della scrittura e lettura da file.
      */
-    public static function readFromFile($filename)
+    class FileManager
     {
-        if (file_exists($filename) && is_readable($filename)) {
-            return file_get_contents($filename);
+        /**
+         * Scrive dei dati in un file.
+         *
+         * @param string $filename Nome del file in cui scrivere i dati.
+         * @param string $data Dati da scrivere nel file.
+         * @param bool $append True se si desidera aggiungere i dati al file esistente, False per sovrascrivere il file.
+         * @return bool True se la scrittura è avvenuta con successo, False altrimenti.
+         */
+        public static function writeToFile($filename, $data, $append = false)
+        {
+            $flags = $append ? FILE_APPEND | LOCK_EX : LOCK_EX;
+            return file_put_contents($filename, $data, $flags) !== false;
         }
-        return false;
+
+        /**
+         * Legge i dati da un file.
+         *
+         * @param string $filename Nome del file da cui leggere i dati.
+         * @return string|false Contenuto del file letto, oppure False in caso di errore.
+         */
+        public static function readFromFile($filename)
+        {
+            if (file_exists($filename) && is_readable($filename)) {
+                return file_get_contents($filename);
+            }
+            return false;
+        }
     }
-}
