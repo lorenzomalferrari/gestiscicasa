6a15fe58ac2829c8c9e6b571b8525e5049d65b14
Lorenzo Malferrari <lorenzo.malferrari@ag2.it>
2024-07-15 14:18:14 +0200

    Aggiunto punto dove applicare i log

--- index.php
+++ index.php
@@ -10,8 +10,10 @@
 	$username = $_SESSION[CONFIG['session']['keys']['USERNAME']];
 
 	if (!checkUsernameAndId($username, $idUsername) && confrontaTimestamp(15, time(), $_SESSION[CONFIG['session']['keys']['LAST_ACTIVITY']])) {
+		//creare un log su DB, se vado in login.php non c'è user quindi IpLog
 		header('Location: ' . 'app/view/login.php');
 	}
 	else {
+		//creare un log su DB, se vado in home, il log da usare è UserLog
 		header('Location: ' . ROOT . 'app/view/home.php');
 	}
