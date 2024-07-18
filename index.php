b16767ed626914911ce2efbf6091c32cfcecb094
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-07-18 11:29:08 +0200

    pulito dai test

--- index.php
+++ index.php
@@ -12,59 +12,9 @@
 	$idUsername = $_SESSION[CONFIG['session']['keys']['IDUSER']] ?? "";
 	$username = $_SESSION[CONFIG['session']['keys']['USERNAME']] ?? "";
 
-
-
-
-
-	$message = "Log generico, test della classe base Log.php";
-	$action = 1;
-	$data = "{ json composto da dati, presunti dati - TEST }";
-	$beforeState = null;
-	$afterState = null;
-
-	$testLog = new DatabaseLog($message, $action, $data, $beforeState, $afterState, null, null, DB->toString());
-	$testLog->writeToFile();
-
-	$fileManager = new FileManager();
-	print_r("<br><br>");
-	print_r($testLog->getMessage());
-	print_r("<br><br>");
-	print_r($testLog->getAction());
-	print_r("<br><br>");
-	print_r($testLog->getData());
-	print_r("<br><br><br><br>");
-	print_r($fileManager->readFromFile(ROOT .  PathAndFilesLog::PATH_DATABASE_ERROR));
-
-
-
-
-
-
-
-
 	if (!checkUsernameAndId($username, $idUsername) && confrontaTimestamp(15, time(), $_SESSION[CONFIG['session']['keys']['LAST_ACTIVITY']])) {
-		//creare un log su DB, se vado in login.php non c'è user quindi IpLog
-		// Esempio di utilizzo
-		$path_log = "";
-
-		/*$message = "";
-		$action = "";
-		$data = "";
-		$beforeState = "";
-		$afterState = "";
-		$customException  = null;
-		$databaseConnectionInfo = DB;
-		$ipAddress = $_SERVER['HTTP_CLIENT_IP'];
-		$userId = $idUsername;
-
-		$userLog = new UserLog($message, $action, $data, $beforeState, $afterState, $customException, $databaseConnectionInfo, $ipAddress, $userId);
-		$userLog->writeToFile();*/
-
-		print_r("app/view/login.php");
-		//header('Location: ' . 'app/view/login.php');
+		header('Location: ' . 'app/view/login.php');
 	}
 	else {
-		//creare un log su DB, se vado in home, il log da usare è UserLog
-		print_r("app/view/home.php");
-		//header('Location: ' . ROOT . 'app/view/home.php');
+		header('Location: ' . ROOT . 'app/view/home.php');
 	}
