69b4c8d868acfa2fc9e26151845d31305d88deb0
Lorenzo Malferrari <lorenzo.malferrari@ag2.it>
2024-07-22 11:15:47 +0200

    Iniziato a lavorare sul imput del CRM

---
+++ app/view/components/form/advanced-form.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html class="no-js" lang="">
+	<head>
+		<?php require_once("head/_head.php"); ?>
+	</head>
+	<body>
+		<!-- Page Loader Start -->
+		<?php
+			//require_once("_preloader.php");
+		?>
+		<!-- Page Loader End -->
+		<a href="#main-wrapper" data-type="section-switch" class="scrollup">
+			<i class="fas fa-angle-double-up"></i>
+		</a>
+		<div id="main-wrapper" class="dsy-main-wrapper">
+			<div class="dsy-page">
+
+				<div class="dsy-page-content vertical-menu">
+					<header class="header">
+						<!-- Inizio menù laterale -->
+						<?php
+						require_once("_menu.php");
+						?>
+						<!-- Fine menù laterale -->
+						<!-- Inizio intestazione -->
+						<?php
+						require_once("_navbar.php");
+						?>
+						<!-- Fine intestazione -->
+					</header>
+
+					<!--=====================================-->
+					<!--=         Breadcrumb Start          =-->
+					<!--=====================================-->
+					<div class="breadcrumbs-wrap">
+						<div class="container-fluid">
+							<div class="row align-items-center">
+								<div class="col-sm-4 col-12">
+									<div class="breadcrumbs-area">
+										<h1>Advanced Form</h1>
+									</div>
+								</div>
+								<div class="col-sm-8 col-12">
+									<div class="breadcrumbs-area text-sm-right">
+										<ul>
+											<li>
+												<a href="#">COMPONENTS</a>
+											</li>
+											<li>
+												<a href="#">FORM</a>
+											</li>
+											<li>Advanced Form</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php require_once(ROOT . 'app/view/components/form/data/input-fields.php'); ?>
+					<!--=====================================-->
+					<!--=       	Footer Start     		=-->
+					<!--=====================================-->
+					<?php require_once("footer.php"); ?>
+				</div>
+			</div>
+		</div>
+		<?php require_once("_script.php"); ?>
+	</body>
+</html>
---
+++ app/view/components/form/data/input-fields.php
@@ -0,0 +1,198 @@
+<!--=====================================-->
+<!--=         	Form Area Start         =-->
+<!--=====================================-->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card basic-form">
+                <div class="card-body">
+                    <h3 class="text-22 text-midnight text-bold mb-4">Bootstrap Form Validation</h3>
+                    <form class="needs-validation" novalidate>
+                        <div class="form-group">
+                            <div class="input-label">
+                                <label>Name</label>
+                            </div>
+                            <input type="text" class="form-control" placeholder="Enter your name" name="name" required>
+                            <div class="invalid-feedback">
+                                Please choose a name.
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-label">
+                                <label>Email or Username</label>
+                            </div>
+                            <input type="email" class="form-control" placeholder="Enter your email address or username" name="email" required>
+                            <div class="invalid-feedback">
+                                Please choose a username.
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-label">
+                                <label>Phone</label>
+                            </div>
+                            <input type="text" class="form-control" placeholder="Enter your Phone" name="phone" required>
+                            <div class="invalid-feedback">
+                                Please choose a Phone.
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-label">
+                                <label>Passcode</label>
+                            </div>
+                            <input type="password" class="form-control" placeholder="Enter your passcode" name="password" required>
+                            <div class="invalid-feedback">
+                                Please choose a password.
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="check1" required>
+                                <label class="form-check-label" for="check1">I agree to Dashy <a href="#">Privacy Policy</a> & <a href="#">Terms</a></label>
+                                <div class="invalid-feedback">
+                                    Please check.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="submit-btn">REGISTER</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card basic-form">
+                <div class="card-body">
+                    <h3 class="text-22 text-midnight text-bold mb-4">Validation Feedback</h3>
+                    <form class="needs-validation" novalidate>
+                        <div class="form-group">
+                            <div class="input-label">
+                                <label>Name</label>
+                            </div>
+                            <input type="text" class="form-control is-valid" value="Dashy" name="name" required>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-label">
+                                <label>Email or Username</label>
+                            </div>
+                            <input type="email" class="form-control is-valid" value="dashy@gmail.com" name="email" required>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-label">
+                                <label>Passcode</label>
+                            </div>
+                            <input type="password" class="form-control is-valid" value="passcode" name="password" required>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="check2" required>
+                                <label class="form-check-label" for="check2">I agree to Dashy <a href="#">Privacy Policy</a> & <a href="#">Terms</a></label>
+                                <div class="valid-feedback">
+                                    Looks good!
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="submit-btn">REGISTER</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card basic-form">
+                <div class="card-body">
+                    <h3 class="text-22 text-midnight text-bold mb-4">Select 2</h3>
+                    <div class="form-group">
+                        <select class="select2" data-placeholder="Select2 with Placeholder">
+                            <option value="">Select2 with Placeholder</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <select class="select2">
+                            <option value="0">Select One</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <select class="select2" multiple data-placeholder="Multiple Select2">
+                            <option value="">Multiple Select2</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                            <option value="4">Four</option>
+                            <option value="5">Five</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card basic-form">
+                <div class="card-body">
+                    <h3 class="text-22 text-midnight text-bold mb-4">Bootstrap Color Picker</h3>
+                    <div class="form-group">
+                        <div id="cp1" class="input-group" title="Using input value">
+                            <input type="text" class="form-control" value="#DD0F20FF" />
+                            <span class="input-group-append">
+                                <span class="input-group-text colorpicker-input-addon"><i></i></span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div id="cp2" class="input-group" title="Using input value" data-color="rgb(241, 138, 49)">
+                            <input type="text" class="form-control" value="#DD0F20FF" />
+                            <span class="input-group-append">
+                                <span class="input-group-text colorpicker-input-addon"><i></i></span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div id="cp3" class="input-group" title="Using input value" data-color="hsl(56, 93%, 63%)">
+                            <input type="text" class="form-control" value="#DD0F20FF" />
+                            <span class="input-group-append">
+                                <span class="input-group-text colorpicker-input-addon"><i></i></span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card basic-form">
+                <div class="card-body">
+                    <h3 class="text-22 text-midnight text-bold mb-4">Flatpickr Date</h3>
+                    <div class="form-group flatpickr-datetime">
+                        <input class="flatpickr-date form-control" type="text" placeholder="Select Date.." readonly="readonly">
+                        <span class="datetime-icon"><i class="fas fa-calendar-alt"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card basic-form">
+                <div class="card-body">
+                    <h3 class="text-22 text-midnight text-bold mb-4">Flatpickr Time</h3>
+                    <div class="form-group flatpickr-datetime">
+                        <input class="flatpickr-time form-control text-left" type="text" placeholder="Select Time.." readonly="readonly">
+                        <span class="datetime-icon"><i class="far fa-clock"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
