67467b85457f63fbe329b98423855f31d21950dc
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-07-23 14:01:28 +0200

    Rimosso file non più necessario

--- public/log/index.php
+++
@@ -1,3 +0,0 @@
-cartella che conterrà i file di log. saranno raggruppati per cartelle e saranno anche presenti degli zip.
-
-Questo file interrogato a video restituisce questo testo seguito da uno script in json.
