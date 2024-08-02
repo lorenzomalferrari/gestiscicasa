b0e8481a91d86d9c755f1eff95cfc26432ab0336
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-08-02 15:20:20 +0200

    Rimosso commento e aggiunto require

--- app/controller/lib/libs.php
+++ app/controller/lib/libs.php
@@ -1,18 +1,4 @@
 <?php declare(strict_types=1);
-
-    /**
-     * File da richiamare ovunque, il quale conterrà
-     * tutti i richiami dei file importati per la gestione del progetto:
-     *      _define.php: Contiene le definizioni utili per la ROOT e PATH e CONFIG
-     *      _functions.php: Funzioni utili per la gestione di controlli
-     *      _create_directories.php: Recupera una funzione per creare e gestire file e cartelle
-     *      _globals.php: Definisce variabili GLOBALI utili ad esempio al DB
-     *      __session_start.php: Gestisce la sessione
-     *      Tables.php: Per ogni tabella a DB esiste una classe con costanti per richiamare nome della tabella e i campi, evitando errori di battitura
-     *      Database.php: Uso della lib per la connessione al DB
-     *      Log.php: Uso della lib per la gestione di tutti i Log del caso
-     *      CustomException.php: Uso della lib per la gesione custom delle eccezioni
-     */
     require_once("define/_define.php");
     require_once("functions/_functions.php");
     require_once("global/_global.php");
@@ -21,11 +7,11 @@
     require_once(ROOT . "app/model/table/Tables.php");
     require_once(ROOT . 'app/model/file/FileManager.php');
     require_once(ROOT . 'app/model/formatter_validator/JSonValidator.php');
+    require_once(ROOT . 'app/controller/formatter_validator/formatterValidator.php');
     require_once(ROOT . 'app/controller/lib/log/_log.php');
     require_once(ROOT . 'app/model/exception/CustomException.php');
     //creo connessione unica al DB
     require_once(ROOT . 'app/model/Database.php');
-
     //verifico o creo le cartelle dei logs
     require_once(ROOT . 'app/controller/lib/file/create_path_and_file.php');
 
