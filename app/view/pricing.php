dc7222709d6e5e6a2b598552edb10bb5be2688b3
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-08-07 08:12:29 +0200

    Sviluppata pagina dei Prezzi

--- app/view/pricing.php
+++ app/view/pricing.php
@@ -1,17 +1,61 @@
-<?php declare(strict_types=1);
-    require_once("../controller/lib/libs.php");
-    $titlePage = "Prezzi";
-    //Gestione di tutte le variabili php da usare a video
-    require_once(ROOT . "app/model/dashboard/getInfoForPricing.php");
+<?php
+
+declare(strict_types=1);
+require_once("../controller/lib/libs.php");
+$titlePage = "Prezzi";
+//Gestione di tutte le variabili php da usare a video
+require_once(ROOT . "app/model/dashboard/getInfoForPricing.php");
 ?>
 <!doctype html>
 <html class="no-js" lang="">
+
 <head>
     <?php require_once(ROOT . 'app/view/components/template/_head.php'); ?>
+
+    <style>
+        .pricing-box {
+            border: 1px solid #ddd;
+            border-radius: 10px;
+            padding: 20px;
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .pricing-box .item-icon {
+            font-size: 3em;
+            margin-bottom: 10px;
+        }
+
+        .pricing-box .item-title {
+            font-size: 1.5em;
+            margin-bottom: 10px;
+        }
+
+        .pricing-box .item-price {
+            font-size: 2em;
+            color: #333;
+            margin-bottom: 10px;
+        }
+
+        .pricing-box .item-btn {
+            font-size: 1em;
+            padding: 10px 20px;
+            background-color: #007bff;
+            color: #fff;
+            border-radius: 5px;
+            text-decoration: none;
+        }
+
+        .pricing-box .item-btn:hover {
+            background-color: #0056b3;
+        }
+    </style>
 </head>
+
 <body>
     <!-- Page Loader Start -->
-    <?php //require_once("/template/_preloader.php"); ?>
+    <?php //require_once("/template/_preloader.php");
+    ?>
     <!-- Page Loader End -->
     <a href="#main-wrapper" data-type="section-switch" class="scrollup">
         <i class="fas fa-angle-double-up"></i>
@@ -22,12 +66,12 @@
                 <header class="header">
                     <!-- Inizio menù laterale -->
                     <?php
-                    require_once(ROOT . "app/view/template/_menu.php");
+                    require_once(ROOT . "app/view/components/template/_menu.php");
                     ?>
                     <!-- Fine menù laterale -->
                     <!-- Inizio intestazione -->
                     <?php
-                    require_once(ROOT . "app/view/template/_navbar.php");
+                    require_once(ROOT . "app/view/components/template/_navbar.php");
                     ?>
                     <!-- Fine intestazione -->
                 </header>
@@ -64,38 +108,102 @@
                 <!--=====================================-->
                 <section class="pricing-plan">
                     <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <div class="pricing-box">
-                                    <div class="item-icon">
-                                        <i class="flaticon-null-24"></i>
-                                    </div>
-                                    <h2 class="item-title">BASE</h2>
-                                    <p>There are many variations of passages of Lorem Ipsum available but the majority is have suffered alteration in that some form by injected humour or randomised words which don't look even slightly.</p>
-                                    <div class="item-price">$19.00</div>
-                                    <a href="#" class="item-btn">SCEGLI</a>
-                                </div>
+                        <div class="card text-center">
+                            <div class="card-header">
+                                <ul class="nav nav-tabs card-header-tabs">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" href="#personale" data-toggle="tab">Personale</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#aziendale" data-toggle="tab">Aziendale</a>
+                                    </li>
+                                </ul>
                             </div>
-                            <div class="col-lg-4">
-                                <div class="pricing-box">
-                                    <div class="item-icon">
-                                        <i class="flaticon-null-23"></i>
-                                    </div>
-                                    <h2 class="item-title">FAMIGLIA</h2>
-                                    <p>There are many variations of passages of Lorem Ipsum available but the majority is have suffered alteration in that some form by injected humour or randomised words which don't look even slightly.</p>
-                                    <div class="item-price">$69.00</div>
-                                    <a href="#" class="item-btn">SCEGLI</a>
+                            <div class="card-body tab-content">
+                                <!-- Prima Tab -->
+                                <div class="tab-pane fade show active" id="personale">
+                                    <section class="pricing-plan mt-4">
+                                        <div class="container-fluid">
+                                            <div class="row">
+                                                <div class="col-lg-4">
+                                                    <div class="pricing-box">
+                                                        <div class="item-icon">
+                                                            <i class="flaticon-null-24"></i>
+                                                        </div>
+                                                        <h2 class="item-title">GRATUITO</h2>
+                                                        <p>There are many variations of passages of Lorem Ipsum available but the majority is have suffered alteration in that some form by injected humour or randomised words which don't look even slightly.</p>
+                                                        <div class="item-price">$0.00</div>
+                                                        <a href="#" class="item-btn">SCEGLI</a>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="pricing-box">
+                                                        <div class="item-icon">
+                                                            <i class="flaticon-null-23"></i>
+                                                        </div>
+                                                        <h2 class="item-title">LIV. 1</h2>
+                                                        <p>There are many variations of passages of Lorem Ipsum available but the majority is have suffered alteration in that some form by injected humour or randomised words which don't look even slightly.</p>
+                                                        <div class="item-price">$9.99</div>
+                                                        <a href="#" class="item-btn">SCEGLI</a>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="pricing-box">
+                                                        <div class="item-icon">
+                                                            <i class="flaticon-null-27"></i>
+                                                        </div>
+                                                        <h2 class="item-title">LIV. 2</h2>
+                                                        <p>There are many variations of passages of Lorem Ipsum available but the majority is have suffered alteration in that some form by injected humour or randomised words which don't look even slightly.</p>
+                                                        <div class="item-price">$19.99</div>
+                                                        <a href="#" class="item-btn">SCEGLI</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </section>
                                 </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="pricing-box">
-                                    <div class="item-icon">
-                                        <i class="flaticon-null-27"></i>
-                                    </div>
-                                    <h2 class="item-title">AZIENDA</h2>
-                                    <p>There are many variations of passages of Lorem Ipsum available but the majority is have suffered alteration in that some form by injected humour or randomised words which don't look even slightly.</p>
-                                    <div class="item-price">$99.00</div>
-                                    <a href="#" class="item-btn">SCEGLI</a>
+
+                                <!-- Seconda Tab -->
+                                <div class="tab-pane fade" id="aziendale">
+                                    <section class="pricing-plan mt-4">
+                                        <div class="container-fluid">
+                                            <div class="row">
+                                                <div class="col-lg-4">
+                                                    <div class="pricing-box">
+                                                        <div class="item-icon">
+                                                            <i class="flaticon-null-24"></i>
+                                                        </div>
+                                                        <h2 class="item-title">PICCOLA</h2>
+                                                        <p>There are many variations of passages of Lorem Ipsum available but the majority is have suffered alteration in that some form by injected humour or randomised words which don't look even slightly.</p>
+                                                        <div class="item-price">$49.99</div>
+                                                        <a href="#" class="item-btn">SCEGLI</a>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="pricing-box">
+                                                        <div class="item-icon">
+                                                            <i class="flaticon-null-23"></i>
+                                                        </div>
+                                                        <h2 class="item-title">MEDIA</h2>
+                                                        <p>There are many variations of passages of Lorem Ipsum available but the majority is have suffered alteration in that some form by injected humour or randomised words which don't look even slightly.</p>
+                                                        <div class="item-price">$69.99</div>
+                                                        <a href="#" class="item-btn">SCEGLI</a>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <div class="pricing-box">
+                                                        <div class="item-icon">
+                                                            <i class="flaticon-null-27"></i>
+                                                        </div>
+                                                        <h2 class="item-title">GRANDE</h2>
+                                                        <p>There are many variations of passages of Lorem Ipsum available but the majority is have suffered alteration in that some form by injected humour or randomised words which don't look even slightly.</p>
+                                                        <div class="item-price">$99.99</div>
+                                                        <a href="#" class="item-btn">SCEGLI</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </section>
                                 </div>
                             </div>
                         </div>
@@ -104,11 +212,11 @@
                 <!--=====================================-->
                 <!--=       	Footer Start     		=-->
                 <!--=====================================-->
-                <?php require_once(ROOT . "app/view/template/_footer.php"); ?>
+                <?php require_once(ROOT . "app/view/components/template/_footer.php"); ?>
             </div>
         </div>
     </div>
-    <?php require_once(ROOT . "app/view/template/_script.php"); ?>
+    <?php require_once(ROOT . "app/view/components/template/_script.php"); ?>
 </body>
 
 </html>
