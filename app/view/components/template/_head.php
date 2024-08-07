363d8de2eea787073071e705426bd017717cb507
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-08-07 07:43:57 +0200

    Aggiunta folder nella path

--- app/view/components/template/_head.php
+++ app/view/components/template/_head.php
@@ -1,5 +1,5 @@
 <?php declare(strict_types=1);
-    require_once("_meta.php");
-    require_once("_css.php");
-    require_once("_font.php");
+    require_once("head/_meta.php");
+    require_once("head/_css.php");
+    require_once("head/_font.php");
 ?>
