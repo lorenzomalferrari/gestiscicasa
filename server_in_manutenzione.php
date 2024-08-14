4cb7a90386be65709c2717f2adddf22d5c946280
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-08-14 20:47:32 +0200

    Creato css dedicata alla pagina di manutenione

--- server_in_manutenzione.php
+++ server_in_manutenzione.php
@@ -1,18 +1,21 @@
-<?php declare(strict_types=1);
-    require_once("app/controller/lib/libs.php");
-    $titlePage = "Server in Manutenzione";
-    $h1 = "SERVER IN MANUTENZIONE";
-    $p1 = "Al momento il servizio non è disponibile";
-    $p2 = "Per maggiori informazioni, potete scriverci a ";
-    $email = CONFIG['site']['email'];
-    $footer = "&copy; 2024 "
-        . CONFIG['site']['name']
-        . " - Autore: "
-        . CONFIG['site']['autore']
-        . " - Versione: " . CONFIG['site']['version'];
+<?php
+
+declare(strict_types=1);
+require_once("app/controller/lib/libs.php");
+$titlePage = "Server in Manutenzione";
+$h1 = "SERVER IN MANUTENZIONE";
+$p1 = "Al momento il servizio non è disponibile";
+$p2 = "Per maggiori informazioni, potete scriverci a ";
+$email = CONFIG['site']['email'];
+$footer = "&copy; 2024 "
+    . CONFIG['site']['name']
+    . " - Autore: "
+    . CONFIG['site']['autore']
+    . " - Versione: " . CONFIG['site']['version'];
 ?>
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,54 +24,10 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.9.1/font/bootstrap-icons.min.css" rel="stylesheet">
-    <style>
-        body {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: Arial, sans-serif;
-            background-color: #f3f3f3;
-            text-align: center;
-        }
-
-        h1 {
-            font-size: 2.5em;
-            margin: 20px 0 10px;
-            color: #333;
-        }
-
-        p {
-            font-size: 1.2em;
-            margin: 5px 0;
-            color: #666;
-        }
-
-        a {
-            font-size: 1em;
-            color: #0066cc;
-            text-decoration: none;
-        }
-
-        a:hover {
-            text-decoration: underline;
-        }
-
-        .footer {
-            font-size: 0.8em;
-            color: #999;
-            margin-top: 20px;
-        }
-
-        .icon {
-            font-size: 15em;
-            /*margin-bottom: 20px;*/
-            color: #666;
-        }
-    </style>
+    <!-- Custom CSS -->
+    <link href="<?php PATH . "public/my_assets/css/server_man.css" ?>" rel="stylesheet">
 </head>
+
 <body>
     <!-- Icona di manutenzione -->
     <i id="maintenance-icon" class="icon"></i>
@@ -111,4 +70,5 @@
         document.getElementById('maintenance-icon').classList.add(randomIcon);
     </script>
 </body>
+
 </html>
