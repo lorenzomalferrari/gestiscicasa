e9b6ed59841b45b2fbde21650477c86849ca8a86
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-08-20 20:33:00 +0200

    Update buttons_list.php

--- app/view/components/table/buttons/buttons_list.php
+++ app/view/components/table/buttons/buttons_list.php
@@ -2,7 +2,7 @@
 <div class="row mb-4">
     <div class="col-12 d-flex justify-content-start">
         <button type="button" onclick="goBack()" class="btn btn-secondary mr-2">Indietro</button>
-        <button type="button" onclick="action('insert',<?php echo $id . ',' . $tableName; ?> )" class="btn btn-primary mr-2">Salva</button>
+        <button type="button" onclick="action('insert',<?php echo $tableName; ?> )" class="btn btn-primary mr-2">Salva</button>
         <button type="button" onclick="goBack('update',<?php echo $id . ',' . $tableName; ?> )" class="btn btn-warning mr-2">Modifica</button>
         <button type="button" onclick="goBack('delete',<?php echo $id . ',' . $tableName; ?> )" class="btn btn-danger">Elimina</button>
     </div>
