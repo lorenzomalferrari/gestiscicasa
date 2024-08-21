4e013da7ee66168fa9a675081ed91d17ef1fe213
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-08-21 13:37:50 +0200

    Aggiunto form per passare tutti i campi

--- app/view/components/table/input-table.php
+++ app/view/components/table/input-table.php
@@ -1,42 +1,42 @@
-<?php
-    //echo "<pre>"; // Iniziamo il blocco di stampa formattata
-    //print_r($fields); // Stampa l'array in formato leggibile
-    //echo "</pre>"; // Chiudiamo il blocco di stampa formattata
-?>
 <div class="container-fluid">
     <?php require(ROOT . "app/view/components/table/buttons/buttons_list.php"); ?>
     <div class="row">
         <div class="col-lg-12">
             <div class="card basic-form">
                 <div class="card-body">
-                    <div class="form-group">
-                        <?php
-                        $html = "";
-                        foreach ($fields['body']['rows'] as $row) {
-                            $html .= '<div class="row">';
-                            foreach ($row as $field) {
-                                $label = $field['label'];
-                                $type = $field['type'];
-                                $attributes = $field['attributes'];
-                                $colSize = isset($field['col']) ? $field['col'] : 12;
-                                // Aggiungi la colonna
-                                $html .= '<div class="col-md-' . $colSize . '">';
-                                $html .= '<div class="input-label">';
-                                $html .= '<label>' . htmlspecialchars($label) . '</label>';
-                                $html .= '</div>';
-                                // Aggiungi l'input
-                                $html .= FormatterValidator::getInputFieldsModal($field);
-                                $html .= '<div class="invalid-feedback">';
-                                $html .= 'Please choose a ' . htmlspecialchars($label) . '.';
-                                $html .= '</div>';
-                                $html .= '</div>'; // Chiudi la colonna
-                            }
+                    <!-- Inizio del form -->
+                    <form id="dynamicForm" method="POST" action="#">
+                        <div class="form-group">
+                            <?php
+                            $html = "";
+                            foreach ($fields['body']['rows'] as $row) {
+                                $html .= '<div class="row">';
+                                foreach ($row as $field) {
+                                    $label = $field['label'];
+                                    $type = $field['type'];
+                                    $attributes = $field['attributes'];
+                                    $colSize = isset($field['col']) ? $field['col'] : 12;
 
-                            $html .= '</div>'; // Chiudi la riga
-                        }
-                        echo $html;
-                        ?>
-                    </div>
+                                    // Aggiungi la colonna
+                                    $html .= '<div class="col-md-' . $colSize . '">';
+                                    $html .= '<div class="input-label">';
+                                    $html .= '<label>' . htmlspecialchars($label) . '</label>';
+                                    $html .= '</div>';
+
+                                    // Aggiungi l'input
+                                    $html .= FormatterValidator::getInputFieldsModal($field);
+
+                                    $html .= '<div class="invalid-feedback">';
+                                    $html .= 'Please choose a ' . htmlspecialchars($label) . '.';
+                                    $html .= '</div>';
+                                    $html .= '</div>'; // Chiudi la colonna
+                                }
+                                $html .= '</div>'; // Chiudi la riga
+                            }
+                            echo $html;
+                            ?>
+                        </div>
+                    </form> <!-- Fine del form -->
                 </div>
             </div>
         </div>
