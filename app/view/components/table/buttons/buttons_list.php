dfc6cd57b8b09be7db0eed4ea9a437830b970e81
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-08-21 13:35:24 +0200

    Predisposto uso dei bottoni

--- app/view/components/table/buttons/buttons_list.php
+++ app/view/components/table/buttons/buttons_list.php
@@ -1,14 +1,15 @@
 <?php
-if( isset($id) 
-$action = "<button type=\"button\" onclick=\"action('insert',<?php echo $tableName; ?> )\" class=\"btn btn-primary mr-2\">Salva</button>";
-else
-$action = "<button type=\"button\" onclick=\"action('update',<?php echo $id . ',' . $tableName; ?> )\" class=\"btn btn-warning mr-2\">Modifica</button>";
+    if( isset($id) )
+        $action = "<button type=\"button\" onclick=\"action('insert', null, '$tableName')\" class=\"btn btn-primary mr-2\">Salva</button>";
+    else {
+        $action = "<button type=\"button\" onclick=\"action('update', $id, '$tableName')\" class=\"btn btn-warning mr-2\">Modifica</button>";
+        $action .= "<button type=\"button\" onclick=\"action('delete', $id, '$tableName')\" class=\"btn btn-danger\">Elimina</button>";
+    }
 ?>
 <!-- Riga per i bottoni -->
 <div class="row mb-4">
     <div class="col-12 d-flex justify-content-start">
         <button type="button" onclick="goBack()" class="btn btn-secondary mr-2">Indietro</button>
         <?php echo $action; ?>
-        <button type="button" onclick="action('delete',<?php echo $id . ',' . $tableName; ?> )" class="btn btn-danger">Elimina</button>
     </div>
 </div>
