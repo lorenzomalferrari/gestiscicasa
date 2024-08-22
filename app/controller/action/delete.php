a3fe7e22ae619f1f65489dc74042549e618a5650
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-08-22 15:26:49 +0200

    Predisposto il delete

---
+++ app/controller/action/delete.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+    header('Content-Type: application/json');
+    require_once('../lib/libs.php');
+
+    // Ricevi i dati JSON inviati in POST
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    // Verifica se i dati JSON sono validi
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        echo json_encode(['success' => false, 'message' => 'Invalid JSON data.']);
+        exit;
+    }
+
+    //print_r($data);
+
+    // Verifica che i dati necessari siano presenti
+    if (isset($data['tableName'])) {
+
+        $params_delete = [
+            ':id' => $data['id']
+        ];
+
+        try {
+            $instances = ClassFactory::create($data['tableName']);
+
+            $delete = "DELETE FROM " . getNomeTabella(CONFIG_ISTANCE->get('TABLEPREFIX'), NomiTabelle::getEnumCaseFromName(strtoupper($data['tableName'])))
+                . " WHERE id = :id ";
+
+            $delete = DB->delete($delete, $params_delete);
+
+        } catch (Exception $e) {
+            echo 'Errore: ' . $e->getMessage();
+        }
+    } else {
+        echo json_encode(['success' => false, 'id' => $new_id, 'message' => 'Missing required data.']);
+    }
