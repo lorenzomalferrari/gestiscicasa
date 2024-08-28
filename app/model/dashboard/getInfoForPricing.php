6e337f9865046737162543ee55d40be900eb99ed
Lorenzo Malferrari <lorenzo.malferrari@ag2.it>
2024-08-28 17:37:59 +0200

    Predisposta logica

--- app/model/dashboard/getInfoForPricing.php
+++ app/model/dashboard/getInfoForPricing.php
@@ -1,6 +1,42 @@
 <?php declare(strict_types=1);
     require_once('../../controller/lib/libs.php');
 
+
+    //Cicla per i tipo di Profilo, al momento della stesura del codice sono:
+    //- Personale
+    //- Aziendale
+
+    $select_profile_types = "";
+
+    // Per ogni voce creo la card
+
+    //Per ogni voce recupero le informazioni da mostrare nei punti:
+    //- Titolo
+    //- Desc
+    //- Prezzo
+
+    //restituire HTML
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     $select = "*";
 
     $query = "SELECT $select FROM " . getNomeTabella(CONFIG_ISTANCE->get('TABLEPREFIX'), EnumTableNames::INFOPROFILETYPES);
