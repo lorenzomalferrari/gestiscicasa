a12ae66aafa97ebeb62c665e516cc5e4afd1b29f
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-09-11 11:41:10 +0200

    Corretta anche icona

--- crm_not_working.php
+++ crm_not_working.php
@@ -43,6 +43,7 @@ $email = CONFIG['site']['email_aziendale'];
 
 <head>
 	<?php require_once(ROOT . 'app/view/components/template/_head.php'); ?>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 
 <body>
@@ -59,7 +60,7 @@ $email = CONFIG['site']['email_aziendale'];
 			<div class="card login-form register-form">
 				<div class="card-body">
 					<div class="item-logo">
-						<i id="maintenance-icon" class="icon"></i>
+						<i id="maintenance-icon" class="icon bi"></i>
 					</div>
 					<h1><?php echo $h1; ?></h1>
 					<div class="text-manatee">
@@ -82,7 +83,12 @@ $email = CONFIG['site']['email_aziendale'];
 		// Seleziona una classe randomicamente dall'array
 		const randomIcon = icons[Math.floor(Math.random() * icons.length)];
 		// Imposta la classe selezionata all'elemento i
-		document.getElementById('maintenance-icon').classList.add(randomIcon);
+		const icon = document.getElementById('maintenance-icon');
+		icon.classList.add(randomIcon);
+		// Aggiungi le dimensioni CSS dinamicamente
+		icon.style.width = '150px';
+		icon.style.height = '150px';
+		icon.style.fontSize = '150px';
 	</script>
 </body>
 
