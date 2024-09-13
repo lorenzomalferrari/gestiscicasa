7a27df09b9b11245cfde340aee8b912ebc7b5394
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-09-13 16:37:07 +0200

    Corretta sintassi

--- public/_dati/db/create_db/Genders.sql
+++ public/_dati/db/create_db/Genders.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS lmgc_Genders (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    gender VARCHAR(50) NOT NULL UNIQUE,
+    gender VARCHAR(50) NOT NULL UNIQUE
 );
+
 /*
 INSERT INTO lmgc_Genders (gender, gender_en, gender_fr, gender_es, gender_de, gender_pt) VALUES
 ('Maschio', 'Male', 'Homme', 'Hombre', 'Männlich', 'Masculino'),
