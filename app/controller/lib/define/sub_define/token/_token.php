d0cfb158398e12dd7177f4a7040bb8a11a1d2877
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-09-15 21:23:21 +0200

    Creata nuova versione del token univoco

--- app/controller/lib/define/sub_define/token/_token.php
+++ app/controller/lib/define/sub_define/token/_token.php
@@ -4,3 +4,19 @@
     * Da migliorare, usando la classe Token che è in sviluppo
     */
     define('UNIQ_TOKEN', uniqid());
+
+    define('UNIQ_TOKEN_v2', [
+        'params' => [
+            CONFIG['site']['name'],
+            CONFIG['site']['version'],
+            CONFIG['db']['tablePrefix'],
+            CONFIG['language']['it'],
+            CONFIG['language']['en'],
+        ],
+        'config' => [
+            'length' => 32, // Lunghezza desiderata del token
+            'algorithm' => HASH_ALGORITHMS['sha512']['sha512'], // Algoritmo di hashing da utilizzare
+            'splitLength' => 4, // Lunghezza dei pezzi in cui suddividere ciascun valore
+            'randomPartLength' => 16 // Lunghezza della parte casuale in byte (default è 16 se non specificato)
+        ],
+    ]);
