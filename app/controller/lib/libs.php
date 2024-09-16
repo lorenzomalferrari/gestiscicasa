4d00603eea884cbb156c84516b43c20e1070065a
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-09-16 09:03:04 +0200

    Riordinati i require

--- app/controller/lib/libs.php
+++ app/controller/lib/libs.php
@@ -1,35 +1,32 @@
 <?php declare(strict_types=1);
 
-use YourNamespace\FileManager;
-
+    //prime definizioni statiche
     require_once("define/_define.php");
+    // funzioni per il controllo e gestione del CRM
     require_once(ROOT . "app/controller/lib/functions/_functions.php");
     require_once(ROOT . "app/controller/lib/global/_global.php");
     require_once(ROOT . "app/controller/lib/session/_session_start.php");
+    require_once(ROOT . 'app/controller/formatter_validator/JSonValidator.php');
+    require_once(ROOT . 'app/controller/factory/ClassFactory.php');
+    require_once(ROOT . 'app/controller/formatter_validator/FormatterInputValidator.php');
+    require_once(ROOT . 'app/controller/lib/log/_log.php');
+    // caricamento delle classi utili al corretto funzionamento del CRM
     require_once(ROOT . 'app/model/log/PathAndFilesLog.php');
     require_once(ROOT . "app/model/table/Tables.php");
     require_once(ROOT . 'app/model/TableNames.php');
-
     require_once(ROOT . 'app/model/token/TokenGenerator.php');
-
-    try {
-    define('UNIQ_TOKEN_V2', TokenGenerator::generate() );
-        echo "Generated Token: $token\n";
-    } catch (Exception $e) {
-        echo "Error: " . $e->getMessage() . "\n";
-    }
-
     require_once(ROOT . 'app/model/file/FileManager.php');
-    require_once(ROOT . 'app/controller/formatter_validator/JSonValidator.php');
-    require_once(ROOT . 'app/controller/formatter_validator/FormatterInputValidator.php');
-    require_once(ROOT . 'app/controller/lib/log/_log.php');
     require_once(ROOT . 'app/model/exception/CustomException.php');
-
     //require classi per la criptazione e uso sicuro dei dati
     require_once(ROOT . "app/model/Crypto.php");
     require_once(ROOT . "app/model/SecureData.php");
 
-    require_once(ROOT . 'app/controller/factory/ClassFactory.php');
+    try {
+        define('UNIQ_TOKEN_V2', TokenGenerator::generate());
+        //echo "Generated Token: $token\n";
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage() . "\n";
+    }
 
     //verifico o creo le cartelle dei logs e del backup
     /*require_once(ROOT . 'app/model/file/FileManager.php');
