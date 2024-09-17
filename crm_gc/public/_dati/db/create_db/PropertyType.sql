c079b0adc969ea85abc0d5a7111cc66294bdd083
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-09-17 16:44:51 +0200

    Aggiornata colonna e nome tabella

--- crm_gc/public/_dati/db/create_db/PropertyType.sql
+++ crm_gc/public/_dati/db/create_db/PropertyType.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS lmgc_PropertyTypes (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    description VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     notes TEXT
 );
 
-INSERT INTO PropertyTypes (description, creationDate, updateDate, notes) VALUES
+INSERT INTO lmgc_PropertyTypes (description, creationDate, updateDate, notes) VALUES
 ('Building', '2024-04-02', NULL, NULL),
 ('Apartment', '2024-04-02', NULL, NULL),
 ('Office', '2024-04-02', NULL, NULL),
