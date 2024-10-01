37bef2c0a5cb77700b94441df62d9fb40370b938
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-10-01 08:31:13 +0200

    SELECT dei settori industriali da poter usare in view

---
+++ crm_gc/app/controller/_select/industry.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+    require_once(ROOT . 'app/model/TableNames.php');
+    require_once(ROOT . "app/model/IndustrySector.php");
+
+    $select = "id, name ";
+
+    $query = "
+        SELECT $select
+        FROM  "
+        . getNomeTabella(CONFIG_ISTANCE->get('TABLEPREFIX'), EnumTableNames::INDUSTRY_SECTOR) . " AS i LIMIT 2";
+
+    $results = DB->selectAll( $query, [] );
+
+    foreach ($results as $r) {
+        $industry_list[] = [
+            $r['id'] => [
+                INPUT_TYPE['elements']['select']['options']['value'] => $r['id'],
+                INPUT_TYPE['elements']['select']['options']['text'] => $r['name']
+            ]
+        ];
+    }
