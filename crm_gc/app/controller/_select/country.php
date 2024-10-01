131de8de014213692a59f4a919bdf29065320c4f
Lorenzo Malferrari <malfe.lore@gmail.com>
2024-10-01 08:30:41 +0200

    SELECT degli stati da usare in view

---
+++ crm_gc/app/controller/_select/country.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+    require_once(ROOT . 'app/model/TableNames.php');
+    require_once(ROOT . "app/model/Country.php");
+
+    $select = "id, name ";
+
+    $query = "
+        SELECT $select
+        FROM  "
+        . getNomeTabella(CONFIG_ISTANCE->get('TABLEPREFIX'), EnumTableNames::COUNTRIES) . " AS c LIMIT 2";
+
+    $results = DB->selectAll( $query, [] );
+
+    foreach ($results as $r) {
+        $countries_list[] = [
+            $r['id'] => [
+                INPUT_TYPE['elements']['select']['options']['value'] => $r['id'],
+                INPUT_TYPE['elements']['select']['options']['text'] => $r['name']
+            ]
+        ];
+    }
